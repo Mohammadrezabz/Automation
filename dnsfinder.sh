d9a3a2a9195ff2110fbeb433a5538e4258e45cc1
Mohammadrezabz <m.boozaridr@gmail.com>
2022-08-29 10:03:39 +0000

    Make dig Command Better

--- dnsfinder.sh
+++ dnsfinder.sh
@@ -2,9 +2,13 @@
 # dig and nslookup dns fetcher
 com=$1
 domain=$2
+#recordtype="${3:-$A}"
+
+echo $recordtype
+
 if [[ $com == "" ]] || [[ $domain == "" ]] || [[ $com == "-h" ]];
 then
-    echo " dig and nslookup dns fetcher
+    echo " dig , nslookup and host dns fetcher
     Usage ./dnsfinder COMMAND DOMAIN < RESOLVERS.TXT
 "
     exit 0
@@ -15,7 +19,7 @@ then
     echo "Running Dig On Target"
     while read x ;
     do
-        $com @$x $domain A | sed '/\n/!s/[0-9.]\+/\n&\n/;/^\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\n/P;D'
+        $com @$x $domain +short
     done
 elif [[ $com == "nslookup" ]];
 then
@@ -24,4 +28,8 @@ then
     do
         $com $domain $x | sed '/\n/!s/[0-9.]\+/\n&\n/;/^\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\n/P;D'
     done
+elif [[ $com == "host" ]];
+then
+    echo "Running Host On Target"
+        $com $domain | sed '/\n/!s/[0-9.]\+/\n&\n/;/^\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}\n/P;D'
 fi
